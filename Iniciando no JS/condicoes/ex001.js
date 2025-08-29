2e147968a488d50b6cf3288dc425f21e8c975b8a
ValdineiCS <109873014+ValdineiCS@users.noreply.github.com>
2025-08-29 17:10:27 -0300

    Proejetinho criado

---
+++ Iniciando no JS/condicoes/ex001.js
@@ -0,0 +1,6 @@
+var vel=50.5;
+if(vel > 60){
+    console.log(`Você está acima da velociade permitida de ${vel}Km/h`)
+}else{
+    console.log('Dirija sempre com segurança! Não esqueça do cinto.')
+}
